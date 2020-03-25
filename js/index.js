0ec55662768cccae5a592138486b3c8b7d8d468e
Gavin Gosling <gavin.gosling@ontariotechu.net>
2020-03-24 20:30:21 -0400

    Main branch positioning fixed, added dynamic options

--- js/index.js
+++ js/index.js
@@ -1,99 +1,164 @@
+// set the dimensions and margins of the diagram
+var margin = {top: 40, right: 90, bottom: 50, left: 90};
+var width = 1280 - margin.left - margin.right;
+var height = 720 - margin.top - margin.bottom;
 
-// Data
-var treeData =
-  {
-    "name": "Top Level",
-    "children": [
-      { 
-		"name": "Level 2: A",
-        "children": [
-          { "name": "Level 3:  A" },
-          { "name": "Level 3:  B" }
-        ]
-      }
-    ]
-  };
-
-
-function update(){
-
-}
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg;
+var results;
+var node;
+var nodes;
+var nodeType = 2;
+var lineType = 0;
 
 function init(){
-  d3.json("/data/CSCI_3090_Final.json").then(function (data) {
+  d3.json("/data/CSCI_3090_Final2.json").then(function (data) {
+    // declares a tree layout and assigns the size
+    results = data;
+    var treemap = d3.tree()
+                    .separation(function(a, b) { return a.parent === b.parent ? 1 : 2; })
+                    .size([width, height]);
+    //  assigns the data to a hierarchy using parent-child relationships
+    nodes = d3.hierarchy(data)
+    // maps the node data to the tree layout
+    nodes = treemap(nodes);
 
-  // set the dimensions and margins of the diagram
-  var margin = {top: 40, right: 90, bottom: 50, left: 90};
-  var width = 1280 - margin.left - margin.right;
-  var height = 720 - margin.top - margin.bottom;
-
-  // declares a tree layout and assigns the size
-  var treemap = d3.tree()
-                  .separation(function(a, b) { return a.parent === b.parent ? 1 : 2; })
-                  .size([width, height]);
-  //  assigns the data to a hierarchy using parent-child relationships
-  var nodes = d3.hierarchy(data)
-            
-
-  // maps the node data to the tree layout
-  nodes = treemap(nodes);
-  var center_x = nodes.x;
-  var center_y = nodes.y;
-  var main_branch = nodes.branch;
+    console.log(nodes);
+    var center_x = nodes.x;
+    var i = 0;
+    nodes.each(d => {
+        if(d.data.isMain == "true"){
+          d.x = center_x;
+        }
+        if(d.data.isMain == "true"){
+          i++;
+        }
   
-  //console.log(nodes);
-  //var center_x = ;
-  //var center_y = ;
-  nodes.each(d => {
-    // Main Branch is the Center
-    // x = center.x 
-    // y = center.y + dy 
-    // Merges Come Back to Center 
-    // x = center.x + dx 
-    // y = center.y + dy
-    // Branches Split off the Center
-    // x = center.x 
-    // y = center.y + dy 
-    /*
-    if(d.branch == main_branch){
-      d.x = center.x;
-    }*/
-  });
-  // append the svg obgect to the body of the page
-  // appends a 'group' element to 'svg'
-  // moves the 'group' element to the top left margin
-  var svg = d3.select("body")
-              .append("svg")
-              .attr("width", width + margin.left + margin.right)
-              .attr("height", height + margin.top + margin.bottom)
-              .append("g")
-              .attr("transform","translate(" + margin.left + "," + margin.top + ")");
-  
-              
-  ///// Lines
-  //boxLine(svg, nodes);
-  //curveLine(svg, nodes);
-  straightLine(svg, nodes);
-  /////
+    });
+    console.log("Number of main:" + i);
+    // Create SVG
+    svg = d3.select("body")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform","translate(" + margin.left + "," + margin.top + ")");
+    
+                
+    ///// Lines
+    straightLine(svg, nodes);
 
-  // adds each node as a group
-  var node = svg.selectAll(".node")
+
+    // adds each node as a group
+    node = svg.selectAll(".node")
               .data(nodes.descendants())
               .enter()
               .append("g")
               .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
               .attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")"; });
 
-  //// Node type
-  //normalNodes(node);
-  //pieNodes(node);
-  arcNodes(svg, 5, nodes.descendants());
-  /////
+    // Creates nodes
+    arcNodes(svg, 5, nodes.descendants());
+    });
+}
 
-  //timeSlider(svg);
-  // Node Text
-  placeText(node, width);
-  });
+function updateNode(input){
+  nodeType = input;
+  update();
+}
+
+function updateLine(input){
+  lineType = input;
+  update();
+}
+
+
+function update(){
+  console.log("Changing lines")
+  // Remove all 
+  d3.select("svg")
+    .selectAll("g")
+    .html("");
+
+  var treemap = d3.tree()
+                  .separation(function(a, b) { return a.parent === b.parent ? 1 : 2; })
+                  .size([width, height]);
+  // assigns the data to a hierarchy using parent-child relationships
+  nodes = d3.hierarchy(results)
+  // maps the node data to the tree layout
+  nodes = treemap(nodes);
+
+  // Nodes
+  if(nodeType == 0){
+    normalNodes(node);
+  } else if (nodeType == 1){
+    arcNodes(svg, 5, nodes.descendants());
+  } else if (nodeType == 2){
+    arcNodes(svg, 0, nodes.descendants());
+  }
+
+  // Links
+  if(lineType == 0){
+    straightLine(svg, nodes);
+  } else if(lineType == 1){
+    curveLine(svg, nodes);
+  } else if(lineType == 2){
+    boxLine(svg, nodes);
+  }
+
+
+  
+}
+
+function curveLine(svg, nodes){
+  var link = svg.selectAll(".link")
+              .data(nodes.descendants().slice(1))
+              .enter()
+              .append("path")
+              .attr("class", "link")
+              .attr("d", function(d) {
+                return "M" + d.x + "," + d.y
+                  + "C" + d.x + "," + (d.y + d.parent.y) / 2
+                  + " " + d.parent.x + "," +  (d.y + d.parent.y) / 2
+                  + " " + d.parent.x + "," + d.parent.y;
+                });
+}
+
+function straightLine(svg, nodes){
+  var link = svg.selectAll("link")
+              .data(nodes.descendants().slice(1))
+              .enter()
+              .append("path")
+              .attr("class", "link")
+      
+  var lines = svg.selectAll('line')
+                .data(nodes.descendants().slice(1))
+                .enter()
+                .append('line')
+                .attr('stroke','#000')
+                .attr('x1',function(d){return d.x})
+                .attr('y1',function(d){return d.y})
+                .attr('x2',function(d){return d.parent.x})
+                .attr('y2',function(d){return d.parent.y})
+}
+
+function boxLine(svg, nodes){
+  svg.selectAll(".link")
+      .data(nodes.descendants().slice(1))
+      .enter()
+      .append("path")
+      .attr("class", "link")
+      .attr("d", function (d) { return box(d); });
+
+}
+
+function box(d){
+  return "M" + d.x + "," + d.y
+          + "L" + d.x + "," + (d.y + d.parent.y) / 2
+          + " " + d.parent.x + "," +  (d.y + d.parent.y) / 2
+          + " " + d.parent.x + "," + d.parent.y;
 }
 
 function normalNodes(node){
@@ -110,52 +175,6 @@ function placeText(node, width){
         return d.children ? width-d.x: width-d.x
       })
       .text(function(d) { return d.data.Author; });
-}
-
-function pan(){
-
-
-}
-
-function findMerge(node){
-
-}
-
-function pieNodes(node){
-  // Data
-  //var data = {a: 9, b: 20, c:30, d:8, e:12}
-  var data = [2, 4, 8, 10];
-  // Arc
-  const arc = d3.arc()
-                .innerRadius(0)
-                .outerRadius(10)
-
-  // Color
-  //var color = d3.scaleOrdinal().domain(["a", "b", "c", "d", "e", "f"]).range(d3.schemeDark2);
-  var color = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c']);
-
-  // Pie
-  var pie = d3.pie()
-  console.log(pie(data));
-  //Generate groups
-  var arcs =  node.selectAll("arc")
-                .data(pie(data))
-                .enter()
-                .append("g")
-                .attr("class", "arc")
-                
-  arcs.append("path")
-      .attr("fill", function(d, i) {
-         
-          return color(i);
-      })
-      .attr("d", arc);
-}
-
-
-function show(){
-
-
 }
 
 function arcNodes(svg, radius, data){
@@ -203,79 +222,11 @@ function arcNodes(svg, radius, data){
   });
 }
 
-function boxLine(svg, nodes){
-  console.log(nodes.descendants().slice(1))
-  var link = svg.selectAll(".link")
-              .data(nodes.descendants().slice(1))
-              .enter()
-              .append("path")
-              .attr("class", "link")
-              .attr("d", function(d) {
-                return "M" + d.x + "," + d.y
-                  + "L" + d.x + "," + (d.y + d.parent.y) / 2
-                  + " " + d.parent.x + "," +  (d.y + d.parent.y) / 2
-                  + " " + d.parent.x + "," + d.parent.y;
-                });
 
-}
 
-function curveLine(svg, nodes){
-  var link = svg.selectAll(".link")
-              .data(nodes.descendants().slice(1))
-              .enter()
-              .append("path")
-              .attr("class", "link")
-              .attr("d", function(d) {
-                return "M" + d.x + "," + d.y
-                  + "C" + d.x + "," + (d.y + d.parent.y) / 2
-                  + " " + d.parent.x + "," +  (d.y + d.parent.y) / 2
-                  + " " + d.parent.x + "," + d.parent.y;
-                });
-}
 
-function straightLine(svg, nodes){
-  var link = svg.selectAll("link")
-              .data(nodes.descendants().slice(1))
-              .enter()
-              .append("path")
-              .attr("class", "link")
-      
-  var lines = svg.selectAll('line')
-                .data(nodes.descendants().slice(1))
-                .enter()
-                .append('line')
-                .attr('stroke','#000')
-                .attr('x1',function(d){return d.x})
-                .attr('y1',function(d){return d.y})
-                .attr('x2',function(d){return d.parent.x})
-                .attr('y2',function(d){return d.parent.y})
-}
 
-function timeSlider(svg){
-   // Vertical
-  var sliderVertical = d3.sliderLeft()
-                         .min(d3.min(100))
-                         .max(d3.max(0))
-                         .height(300)
-                         .tickFormat(d3.format('.2%'))
-                         .ticks(5)
-                         .default(0.015)
-						/*
-                        .on('onchange', val => {
-                          d3.select('p#value-vertical').text(d3.format('.2%')(val));
-                        });*/
-  var gVertical = svg.append('g')
-					 .attr('transform', 'translate(60,30)');
-                      
-  gVertical.call(sliderVertical);
-  svg.append("text")
-	  .attr("dy", "1em")
-	  .attr("y", 0)
-	  .attr("x", 0)
-	  .text("TIME");
-  
- //d3.select('p#value-vertical').text(d3.format('.2%')(sliderVertical.value()));
-} 
+
 
 
 
--- js/treeMaker.js
+++ js/treeMaker.js
@@ -1,4 +1,4 @@
-d3.dsv("\t", "CSCI_3090_Final.tsv").then(function (data) {
+d3.dsv("\t", "SushiGoMk1.tsv").then(function (data) {
   data.forEach(e => {
     e.parents = eval(e.parents);
     e.changes = eval(e.changes);
@@ -6,14 +6,21 @@ d3.dsv("\t", "CSCI_3090_Final.tsv").then(function (data) {
     e.deletions = eval(e.deletions);
     e.parents = eval(e.parents);
     e.files = eval(e.files);
+    e.isMainBranch = (e.branch === "master")?true:false;
   });
   findChildren(data);
-  console.log(data[0]);
-  console.log(data);
+  // console.log(data[0]);
+  // console.log(data);
   tree = makeTree(data);
-  console.log(tree);
-  console.log(JSON.stringify(tree));
+  // console.log(tree);
+  // console.log(JSON.stringify(tree));
+  sunburst = makeSunburst(data);
+  // console.log(JSON.stringify(sunburst));
+  download(JSON.stringify(sunburst), 'SushiGoBranches.json');
   // download(JSON.stringify(tree), 'CSCI_3090_Final.json');
+  // download(JSON.stringify(sunburst), 'SQA_Project_Sunburst.json');
+  // download(JSON.stringify(sunburst), 'SushiGo.json');
+  // download(JSON.stringify(sunburst), 'Sunburst_Test.json');
   
 });
 
@@ -58,6 +65,37 @@ function makeTree(data) {
   return treeData;
 }
 
-function makeRecursiveTree(inData, index) {
+function makeSunburst(inData) {
+  authors = new Set();
+  //make the top level
+  sunburstData = {name: "RepoName", children: [] };
+  console.log("level 1 ");
+  console.log(sunburstData);
+  //get all the authors
+  inData.forEach(element => {
+    authors.add(element.author);
+  });
+  //make second level the authors
+  authors.forEach(element => {
+    sunburstData.children.push({author: element, children : []});
+  });
+  
+  console.log("level 2 ");
+  console.log( sunburstData);
+  inData.forEach(element => {
+    // console.log(element.author);
+    // console.log(sunburstData.children[element.author]);
+    sunburstData.children.forEach(e => {
+      if(e.author === element.author){
+        e.children.push(element);
+        console.log(e.children);
+      }
+      // console.log(element.author);
+      // console.log(e.author);
+    });
+  });
+  console.log("level 3 ");
+  console.log(sunburstData);
+  return sunburstData;
 }
 
